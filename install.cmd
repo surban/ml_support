61cc7086f3f8e975480ab2c5d71260fca0d14cb5
Sebastian Urban <surban@tum.de>
2014-03-03 19:24:39 +0100

    initial commit

---
+++ install.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+echo Installing cudamat
+pushd cudamat
+call pip install --user -e .
+if not errorlevel 0 goto :eof
+popd
+
+echo Installing gnumpy
+pushd gnumpy
+call pip install --user -e .
+if not errorlevel 0 goto :eof
+popd
+
+echo Installing PyCUDA
+pushd pycuda
+call install.cmd
+if not errorlevel 0 goto :eof
+popd
+
+echo Installing Theano
+pushd Theano
+call pip install --user -e .
+if not errorlevel 0 goto :eof
+popd
+
+echo ml_support was installed successfully.
+
+
