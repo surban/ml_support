f28a3f057f5301623d2c89102013be4f4503a344
Sebastian Urban <surban@tum.de>
2014-03-05 11:51:26 +0100

    install

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+echo Installing cudamat
+pushd cudamat
+pip install --user -e .
+popd
+
+echo Installing gnumpy
+pushd gnumpy
+pip install --user -e .
+popd
+
+echo Installing PyCUDA
+pushd pycuda
+./install.sh
+popd
+
+echo Installing Theano
+pushd Theano
+pip install --user -e .
+popd
+
+echo ml_support was installed successfully.
+
+
